811f180d37ecdd632bf70dac70f08ae76d1770e4
nejat-njonjo <nejatmhango@gmail.com>
2021-02-16 23:31:01 +0200

    update

--- public/script.js
+++ public/script.js
@@ -1,9 +1,6 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
-const myPeer = new Peer(undefined, {
-  host: '/',
-  port: '3001'
-})
+const myPeer = new Peer(undefined)
 const peers = {}
 
 const myVideo = document.createElement('video')
