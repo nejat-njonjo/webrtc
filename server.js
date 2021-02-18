479a131ed33817a7b21d41116e40aa5b30b07cd3
nejat-njonjo <nejatmhango@gmail.com>
2021-02-18 15:06:02 +0200

    added json encoder

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
+
 const routes = require('./routes')
 const WebSocketService = require('./services/websocket-service')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
-app.get('/', routes)
+app.use('/', routes)
 new WebSocketService(io)
 
 server.listen(process.env.PORT || '3000')
