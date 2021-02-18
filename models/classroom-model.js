917b89f9f00bda108826164a89ba3b05a3341a2d
nejat-njonjo <nejatmhango@gmail.com>
2021-02-18 17:58:45 +0200

    seperated concerns

--- models/classroom-model.js
+++ models/classroom-model.js
@@ -1,27 +1,4 @@
-const db = require('./db')
-
-const AttendantSchema = new db.Schema({
-  name: String
-})
-
-const ClassroomSchema = new db.Schema({
-  class: String,
-  subject: String,
-  status: {
-    type: String,
-    default: 'active'
-  },
-  topic: String,
-  description: String,
-  attendants: [AttendantSchema],
-  host: {
-    type: db.Schema.Types.ObjectId,
-    ref: 'User'
-  },
-  key: String
-})
-
-const classroomModel = db.model('classroom', ClassroomSchema)
+const classroomModel = require('./class-schema')
 
 async function create(payload) {
   const result = await classroomModel.create(payload)
