efb89f9649c42e6d9a7b922fc925b9400c169ca3
nejat-njonjo <nejatmhango@gmail.com>
2021-02-18 22:00:31 +0200

    added more routes

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,20 @@ router.get('/', (req, res) => {
   res.render('login')
 })
 
+router.get('/home', (req, res) => {
+  res.render('home',)
+})
+
+router.get('/rooms', (req, res) => {
+  res.render('classroom', {
+    roomId: null
+  })
+})
+
 router.get('/login', (req, res) => {
   res.render('login')
 })
+router.post('/login', userController.login)
 
 router.get('/register', (req, res) => {
   res.render('register')
@@ -21,8 +32,9 @@ router.post('/user', userController.createUser)
 router.get('/user', userController.checkUser)
 
 router.post('/classroom', (req, res) => {
-  console.log(req.body)
   res.send(req.body)
 })
+
+router.post('/session/:_id', userController.isLoggedIn)
 
 module.exports = router
