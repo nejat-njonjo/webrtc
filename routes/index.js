da9944f743beb58a14a19d562fe22a6361cebfa4
nejat-njonjo <nejatmhango@gmail.com>
2021-02-18 18:00:01 +0200

    added routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,24 @@
 const router = require('express').Router()
+const classroomController = require('../controllers/classroom-controller')
+const userController = require('../controllers/user-controller')
 
 /**
  * Index Route
  */
 router.get('/', (req, res) => {
-  res.render('index')
+  res.render('login')
 })
+
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/user', userController.createUser)
+router.get('/user', userController.checkUser)
 
 router.post('/classroom', (req, res) => {
   console.log(req.body)
