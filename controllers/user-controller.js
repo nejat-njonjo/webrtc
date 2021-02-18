a847441791b72258a04de69c95cc3c357e27a2ac
nejat-njonjo <nejatmhango@gmail.com>
2021-02-18 17:58:20 +0200

    refactored user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,7 +3,7 @@ const userModel = require('../models/user-model')
 async function createUser(req, res) {
   try {
     const keys = Object.keys(req.body)
-    const permits = ['name', 'username', 'role', 'password']
+    const permits = ['name', 'username', 'password']
 
     if (keys.length < permits.length || keys.length > permits.length) {
       res.send({error: "incorrect fields"})
@@ -11,22 +11,22 @@ async function createUser(req, res) {
     }
 
     for (field of keys) {
-      if(!req.body[key]) {
+      if(!req.body[field]) {
         res.send({ error: "incorrect fields" })
         break
       }
     }
 
     const payload = await userModel.create(req.body)
-    res.send(payload)
+    res.status(201).send(payload)
   } catch (error) {
-    res.send({error: 'Cannot create user'})
+    res.send({error: 'Cannot create user: '+ error})
   }
 }
 
 async function checkUser(req, res) {
   try {
-    const result = await userModel.find(req.body)
+    const result = await userModel.find(req.query)
     res.send(result)
   } catch (error) {
     res.send({ error: 'Cannot create user' })
