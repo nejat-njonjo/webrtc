dc2b30e62be753289d89fe72475057a1f6e7b7c0
nejat-njonjo <nejatmhango@gmail.com>
2021-02-19 15:21:35 +0200

    refacotred model

--- models/classroom-model.js
+++ models/classroom-model.js
@@ -1,17 +1,23 @@
 const classroomModel = require('./class-schema')
 
 async function create(payload) {
-  const result = await classroomModel.create(payload)
+  const result = await (await classroomModel.create(payload)).populate({path: 'host', select: '-password'})
   return result
 }
 
 async function find(query = null) {
   const result = await classroomModel.find(query ? query : {})
   return result
-} 
+}
+
+async function row(query) {
+  const result = await classroomModel.findOne(query).populate({ path: 'host', select: '-password' })
+  return result
+}
 
 module.exports = Object.freeze({
   create,
-  find
+  find,
+  row
 })
 
