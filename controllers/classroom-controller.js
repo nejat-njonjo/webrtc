eb6b6b4d50b845262c74a03f49024ccbefd2bbf8
nejat-njonjo <nejatmhango@gmail.com>
2021-02-19 15:21:07 +0200

    modified schema

--- controllers/classroom-controller.js
+++ controllers/classroom-controller.js
@@ -1,31 +1,63 @@
 const classroomModel = require('../models/classroom-model')
-const uuid = require('uuid')
+const {uuid} = require('uuid')
 
-async function getClasses(req, res) {
+const randomChars = max => {
+  let randomized = ''
+  const rand = Math.random().toString(36)
+  if (parseInt(max) <= 3) {
+    randomized = `${rand.substring(2, 4)}${rand.substring(2, 4)}`
+  } else {
+    randomized = `${rand.substring(2, max)}${rand.substring(2, max)}`
+  }
+
+  return randomized.substr(0, max)
+}
+
+async function getHostRoom(req, res) {
   try {
-    const classes = classroomModel.find()
-    res.send(classes)
+    const room = await classroomModel.row({key: req.params.roomKey})
+    res.render('classroom', {
+      roomId: req.params.roomKey,
+      room
+    })
   } catch (error) {
     res.send({error: 'Error occured' + error.message})
   }
 }
 
-async function createClassroom(req, res) {
+async function getRooms(req, res) {
   try {
-    const data = req.body
-
-    Object.assign(data, {
-      key: uuid()
+    const rooms = classroomModel.find().populate('host')
+    res.render('room', {
+      rooms
     })
-
-    const classroom = await classroomModel.create(req.body)
-    res.send(classroom)
   } catch (error) {
-    res.send({error: 'could not create classroom'})
+    res.send({ error: 'Error occured' + error.message })
+  }
+}
+
+async function createRoom(req, res) {
+  try {
+    const { className, subject, topic, description, user} = req.body
+    const data = {
+      className,
+      subject,
+      topic,
+      description,
+      key: `${randomChars(10)}-${randomChars(10)}-${randomChars(10)}-${randomChars(10)}${randomChars(10)}-${randomChars(10)}`,
+      host: user._id
+    }
+
+    const room = await classroomModel.create(data)
+
+    res.send(room)
+  } catch (error) {
+    res.send({error: 'could not create classroom: '+ error},)
   }
 }
 
 module.exports = Object.freeze({
-  getClasses,
-  createClassroom
+  getHostRoom,
+  createRoom,
+  getRooms
 })
